b1a2d90afbe5f6b723484d39489ef658b34b4823
Kipper88 <kozlenkoivan88@gmail.com>
2025-06-27 00:36:13 +0300

    lalla

--- main.py
+++ main.py
@@ -1,32 +1,80 @@
+import logging
 from fastapi import FastAPI, Request, Response
 from utils import *
-from cfg import directions
+from cfg import directions, f12796_dict, f12798_dict, f12799_dict, f12802_dict
+
+import sys
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("webhook")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+logger.propagate = False
 
 app = FastAPI()
 
 @app.post("/api/deal")
 async def receive_webhook(request: Request):
-    form = await request.form()
-    data = dict(form) 
-    
-    id_deal_bx24 = data.get("data[FIELDS][ID]", "")
-    data_bx24 = await get_data_from_bx24(id_deal_bx24)
-    
-    id_worker_bx24 = data_bx24.get("ASSIGNED_BY_ID", "")
-    name_bx24 = await get_worker_from_bx24(id_worker_bx24)
-    
-    id_worker_rukovoditel = await get_worker_from_rukovoditel(name_bx24)
-    
-    route =                 routes.get(str(data_bx24.get("UF_CRM_683EA5EE6C732", "")[0]))
-    direction =             directions.get(str(data_bx24.get("UF_CRM_683EA5EE78933", "")[0]))
-    inn =                   await get_inn_bx24(data_bx24.get("COMPANY_ID", ""))
-    btg_manager_kam =       id_worker_rukovoditel
-    comment_on_the_deal =   str(data_bx24.get("COMMENTS", "")).replace("[p]", '').replace('[/p]', '')
+    try:
+        form = await request.form()
+        data = dict(form)
+        logger.info(f"Получены данные webhook: {data}")
 
-    the_customer_company =  await get_company_from_rukovoditel(inn)
-    
-    #print(f"route: {route};\ndirection: {direction};\ninn: {inn};\nbtg_manager_kam: {btg_manager_kam};\ncomment_on_the_deal: {comment_on_the_deal};\nthe_customer_company: {the_customer_company};")
-    
-    await post_data_to_ruk(route, direction, inn, btg_manager_kam, comment_on_the_deal, the_customer_company)
+        id_deal_bx24 = data.get("data[FIELDS][ID]", "")
+        logger.info(f"ID сделки: {id_deal_bx24}")
+
+        data_bx24 = await get_data_from_bx24(id_deal_bx24)
+        logger.info(f"Получены данные сделки из BX24")
+
+        id_worker_bx24 = data_bx24.get("ASSIGNED_BY_ID", "")
+        name_bx24 = await get_worker_from_bx24(id_worker_bx24)
+        id_worker_rukovoditel = await get_worker_from_rukovoditel(name_bx24)
         
-    return Response(status_code=200)
+        route = routes.get(str(data_bx24["UF_CRM_683EA5EE6C732"][0]) if data_bx24["UF_CRM_683EA5EE6C732"] else "")
+        direction = directions.get(str(data_bx24["UF_CRM_683EA5EE78933"][0]) if data_bx24["UF_CRM_683EA5EE78933"] else "")
+        inn = await get_inn_bx24(data_bx24.get("COMPANY_ID", ""))
+        comment_on_the_deal = str(data_bx24.get("COMMENTS", "")).replace("[p]", '').replace('[/p]', '')
+        btg_manager_kam = id_worker_rukovoditel
+
+        logger.info(f"Основные поля: route={route}, direction={direction}, inn={inn}")
+
+        f12795 = data_bx24.get("UF_CRM_683EA5EE8F634", "")
+        f12796 = f12796_dict.get(str(data_bx24["UF_CRM_685000BC67768"][0]) if data_bx24["UF_CRM_685000BC67768"] else "")
+        f12797 = data_bx24.get("UF_CRM_1750405403", "")
+        f12798 = f12798_dict.get(data_bx24.get("UF_CRM_1750750718"))
+        f12799 = f12799_dict.get(data_bx24.get("UF_CRM_1750751963"))
+        f12800 = data_bx24.get("UF_CRM_1750752148", "")
+        f12801 = data_bx24.get("UF_CRM_1750755583", "")
+        f12802 = f12802_dict.get(data_bx24.get("UF_CRM_1750755621"))
+        f12803 = data_bx24.get("UF_CRM_1750755714", "")
+        f12804 = data_bx24.get("UF_CRM_1750755728", "")
+        f12805 = data_bx24.get("UF_CRM_1750755776", "")
+        f12806 = data_bx24.get("UF_CRM_1750755907", "")
+        f12807 = data_bx24.get("UF_CRM_1750755925", "")
+        f12808 = data_bx24.get("UF_CRM_1750755957", "")
+        f12809 = await get_worker_from_rukovoditel(await get_worker_from_bx24(data_bx24.get("UF_CRM_1750756012", "")))
+        f12810 = await get_company_from_rukovoditel_btg_company(await get_inn_bx24(data_bx24.get("UF_CRM_1750756090", "")))
+        f12811 = data_bx24.get("UF_CRM_1750756250", "")
+        f12812 = data_bx24.get("UF_CRM_1750756289", "")
+        f12813 = data_bx24.get("UF_CRM_1750756299", "")
+        f12814 = data_bx24.get("UF_CRM_1750756532", "")
+        f12815 = data_bx24.get("UF_CRM_1750756900", "")
+        f12816 = data_bx24.get("UF_CRM_1750756924", "")
+        f12817 = "\n".join(f"https://btg24.bitrix24.ru{f['downloadUrl']}" for f in data_bx24.get("UF_CRM_1750756580", []) if isinstance(f, dict) and 'downloadUrl' in f)
+
+        the_customer_company = await get_company_from_rukovoditel(inn)
+        logger.info(f"Отправка данных в Руково...")
+
+        await post_data_to_ruk(route, direction, inn, btg_manager_kam, comment_on_the_deal, the_customer_company,
+            f12795, f12796, f12797, f12798, f12799, f12800, f12801, f12802, f12803, f12804, f12805, f12806, f12807, f12808,
+            f12809, f12810, f12811, f12812, f12813, f12814, f12815, f12816, f12817)
+
+        logger.info(f"Данные успешно отправлены.")
+        return Response(status_code=200)
+    
+    except Exception as e:
+        logger.exception(f"Ошибка при обработке webhook: {e}")
+        return Response(status_code=500, content="Ошибка на сервере")
--- utils.py
+++ utils.py
@@ -32,7 +32,8 @@ async def get_resp_without_filter(entity_id, select_fields, filters=None):
         
         return data
 
-async def post_data_to_ruk(route, direction, inn, btg_manager_kam, comment_on_the_deal, the_customer_company):
+async def post_data_to_ruk(route, direction, inn, btg_manager_kam, comment_on_the_deal, the_customer_company,\
+    f12795, f12796, f12797, f12798, f12799, f12800, f12801, f12802, f12803, f12804, f12805, f12806, f12807, f12808, f12809, f12810, f12811, f12812, f12813, f12814, f12815, f12816, f12817):
     params = {
         'key': apiKey,
         'username': username,
@@ -47,6 +48,30 @@ async def post_data_to_ruk(route, direction, inn, btg_manager_kam, comment_on_th
             'field_12780': comment_on_the_deal,
             'field_12782': (the_customer_company),
             #'field_559': '318',
+            
+            "field_12795": f12795,
+            "field_12796": f12796,
+            "field_12797": f12797,
+            "field_12798": f12798,
+            "field_12799": f12799,
+            "field_12800": f12800,
+            "field_12801": f12801,
+            "field_12802": f12802,
+            "field_12803": f12803,
+            "field_12804": f12804,
+            "field_12805": f12805,
+            "field_12806": f12806,
+            "field_12807": f12807,
+            "field_12808": f12808,
+            "field_12809": f12809,
+            "field_12810": f12810,
+            "field_12811": f12811,
+            "field_12812": f12812,
+            "field_12813": f12813,
+            "field_12814": f12814,
+            "field_12815": f12815,
+            "field_12816": f12816,
+            "field_12817": f12817,
         }      
     }
     
@@ -69,6 +94,11 @@ async def get_worker_from_rukovoditel(name_bx24):
         
 async def get_company_from_rukovoditel(inn):
     data = await get_resp_without_filter("68", "1004", filters = {"1004": inn})
+
+    return data[0].get('id')
+
+async def get_company_from_rukovoditel_btg_company(inn):
+    data = await get_resp_without_filter("66", "880", filters = {"880": inn})
 
     return data[0].get('id')
     
